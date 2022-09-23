c8d0f5fd6324e78f575de2a34305b1935b0c4162
pRob3 <bubblan@slaf.se>
2022-09-23 07:58:09 +0200

    React vs. HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [myDate, myDateUpdate] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      myDateUpdate(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Example HTML</h3>
+      <input />
+      <div>{myDate.toString()}</div>
     </div>
   );
 }
