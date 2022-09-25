87a26cf98edcd47fe48d7f64745e3f6b3b1b5c47
pRob3 <bubblan@slaf.se>
2022-09-25 19:48:31 +0200

    TypeAhead for actors

--- src/movies/CreateMovie.tsx
+++ src/movies/CreateMovie.tsx
@@ -1,7 +1,30 @@
+import { genreDTO } from './genres/genres.model';
+import MovieForm from './MovieForm';
+import { movieTheaterDTO } from './movietheaters/movieTheater.model';
+
 export default function CreateMovie() {
+  const nonSelectedGenres: genreDTO[] = [
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Comedy' },
+  ];
+
+  const nonSelectedMovieTheaters: movieTheaterDTO[] = [
+    { id: 1, name: 'Bergakungen' },
+    { id: 2, name: 'Filmstaden' },
+  ];
+
   return (
     <>
       <h3>Create Movie</h3>
+      <MovieForm
+        model={{ title: '', inTheaters: false, trailer: '' }}
+        onSubmit={(values) => console.log(values)}
+        nonSelectedGenres={nonSelectedGenres}
+        selectedGenres={[]}
+        nonSelectedMovieTheaters={nonSelectedMovieTheaters}
+        selectedMovieTheaters={[]}
+        selectedActors={[]}
+      />
     </>
   );
 }
--- src/movies/EditMovie.tsx
+++ src/movies/EditMovie.tsx
@@ -1,7 +1,35 @@
+import { genreDTO } from './genres/genres.model';
+import MovieForm from './MovieForm';
+import { movieTheaterDTO } from './movietheaters/movieTheater.model';
+
 export default function EditMovie() {
+  const nonSelectedGenres: genreDTO[] = [{ id: 2, name: 'Comedy' }];
+  const selectedGenres: genreDTO[] = [{ id: 1, name: 'Action' }];
+
+  const nonSelectedMovieTheaters: movieTheaterDTO[] = [
+    { id: 2, name: 'Filmstaden' },
+  ];
+  const selectedMovieTheaters: movieTheaterDTO[] = [
+    { id: 1, name: 'Bergakungen' },
+  ];
+
   return (
     <>
       <h3>Edit Movie</h3>
+      <MovieForm
+        model={{
+          title: 'Kung Fury',
+          inTheaters: false,
+          trailer: 'url',
+          releaseDate: new Date('2015-05-22T00:00:00'),
+        }}
+        onSubmit={(values) => console.log(values)}
+        nonSelectedGenres={nonSelectedGenres}
+        selectedGenres={selectedGenres}
+        nonSelectedMovieTheaters={nonSelectedMovieTheaters}
+        selectedMovieTheaters={selectedMovieTheaters}
+        selectedActors={[]}
+      />
     </>
   );
 }
