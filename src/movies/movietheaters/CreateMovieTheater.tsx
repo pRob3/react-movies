72040cf6fe0dd754608d3a31aa618c205d064fd4
pRob3 <bubblan@slaf.se>
2022-09-25 17:54:07 +0200

    Integrating Formik and Leaflet

--- src/movies/movietheaters/CreateMovieTheater.tsx
+++ src/movies/movietheaters/CreateMovieTheater.tsx
@@ -1,10 +1,15 @@
 import { useHistory } from 'react-router-dom';
+import MovieTheaterForm from './MovieTheaterForm';
 
 export default function CreateMovieTheater() {
   const history = useHistory();
   return (
     <>
       <h3>Create Movie Theaters</h3>
+      <MovieTheaterForm
+        model={{ name: '' }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
--- src/movies/movietheaters/EditMovieTheater.tsx
+++ src/movies/movietheaters/EditMovieTheater.tsx
@@ -1,11 +1,19 @@
 import { useParams } from 'react-router-dom';
+import MovieTheaterForm from './MovieTheaterForm';
 
 export default function EditMovieTheater() {
   const { id }: any = useParams();
   return (
     <>
       <h3>Edit Movie Theater</h3>
-      The id is {id}
+      <MovieTheaterForm
+        model={{
+          name: 'Bergakungen',
+          latitude: 57.7021247429003,
+          longitude: 11.98610544204712,
+        }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
