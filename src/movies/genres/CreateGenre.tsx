9a55cc3bf4e77cd7f5e79d9366e34dc25b97dc5c
pRob3 <bubblan@slaf.se>
2022-09-25 15:21:20 +0200

    Creating the Edit Genres Form

--- src/movies/genres/CreateGenre.tsx
+++ src/movies/genres/CreateGenre.tsx
@@ -1,41 +1,19 @@
-import { Form, Formik } from 'formik';
-import { Link, useHistory } from 'react-router-dom';
-import Button from '../../utils/Button';
-import * as Yup from 'yup';
-import TextField from '../../forms/TextField';
+import GenreForm from './GenreForm';
 
 export default function CreateGenre() {
-  const history = useHistory();
+  // const history = useHistory();
   return (
     <>
       <h3>Create Genre</h3>
 
-      <Formik
-        initialValues={{ name: '' }}
+      <GenreForm
+        model={{ name: '' }}
         onSubmit={async (value) => {
-          // When the form is ssubmitted
-          await new Promise((r) => setTimeout(r, 1));
+          // When the form is submitted
+          await new Promise((resolve) => setTimeout(resolve, 1));
           console.log(value);
         }}
-        validationSchema={Yup.object({
-          name: Yup.string()
-            .required('This field is required')
-            .firstLetterUppercase(),
-        })}
-      >
-        {(formikProps) => (
-          <Form>
-            <TextField field='name' displayName='Name' />
-
-            <Button disabled={formikProps.isSubmitting} type='submit'>
-              Save Changes
-            </Button>
-            <Link className='btn btn-secondary' to='/genres'>
-              Cancel
-            </Link>
-          </Form>
-        )}
-      </Formik>
+      />
     </>
   );
 }
--- src/movies/genres/EditGenre.tsx
+++ src/movies/genres/EditGenre.tsx
@@ -1,11 +1,20 @@
 import { useParams } from 'react-router-dom';
+import GenreForm from './GenreForm';
 
 export default function EditGenre() {
   const { id }: any = useParams();
   return (
     <>
       <h3>Edit Genre</h3>
-      The id is {id}
+      <GenreForm
+        model={{ name: 'Action' }}
+        onSubmit={async (value) => {
+          // When the form is submitted
+          await new Promise((resolve) => setTimeout(resolve, 1));
+          console.log(id);
+          console.log(value);
+        }}
+      />
     </>
   );
 }
