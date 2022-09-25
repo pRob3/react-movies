dfce89035b9c52377d364093a742a1138d7b6aa4
pRob3 <bubblan@slaf.se>
2022-09-25 16:40:16 +0200

    Actor form

--- src/movies/actors/CreateActor.tsx
+++ src/movies/actors/CreateActor.tsx
@@ -1,11 +1,16 @@
 import { useHistory } from 'react-router-dom';
+import ActorForm from './ActorForm';
 
 export default function CreateActor() {
   const history = useHistory();
   return (
     <>
       <h3>Create Actor</h3>
-      Save Changes
+
+      <ActorForm
+        model={{ name: '', dateOfBirth: undefined }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
--- src/movies/actors/EditActor.tsx
+++ src/movies/actors/EditActor.tsx
@@ -1,11 +1,22 @@
 import { useParams } from 'react-router-dom';
+import ActorForm from './ActorForm';
 
 export default function EditActor() {
   const { id }: any = useParams();
   return (
     <>
       <h3>Edit Actor</h3>
-      The id is {id}
+      <ActorForm
+        model={{
+          name: 'Danny McBride',
+          dateOfBirth: new Date('1976-12-29T00:00:00'),
+          biography:
+            'Danny McBride is an **American** actor, writer, and producer.',
+          pictureURL:
+            'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/sVFPzl2t5QlGudQKWAszIWZCXZv.jpg',
+        }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
