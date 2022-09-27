ba2915925827393415bb4f5b67cb8261d3d920c1
pRob3 <bubblan@slaf.se>
2022-09-27 11:10:44 +0200

    Paginating Genres

--- src/movies/genres/IndexGenres.tsx
+++ src/movies/genres/IndexGenres.tsx
@@ -4,15 +4,30 @@ import { Link } from 'react-router-dom';
 import { urlGenres } from '../../endpoints';
 import Button from '../../utils/Button';
 import GenericList from '../../utils/GenericList';
+import Pagination from '../../utils/Pagination';
+import RecordsPerPageSelect from '../../utils/RecordsPerPageSelect';
 import { genreDTO } from './genres.model';
 
 export default function IndexGenres() {
   const [genres, setGenres] = useState<genreDTO[]>([]);
+  const [totalAmountOfPages, setTotalAmountOfPages] = useState(0);
+  const [recordsPerPage, setRecordsPerPage] = useState(5);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    axios.get(urlGenres).then((response: AxiosResponse<genreDTO[]>) => {
-      setGenres(response.data);
-    });
-  }, []);
+    axios
+      .get(urlGenres, {
+        params: { page, recordsPerPage },
+      })
+      .then((response: AxiosResponse<genreDTO[]>) => {
+        const totalAmountOfRecords = parseInt(
+          response.headers['totalamountofrecords'],
+          10
+        );
+        setTotalAmountOfPages(Math.ceil(totalAmountOfRecords / recordsPerPage));
+        setGenres(response.data);
+      });
+  }, [page, recordsPerPage]);
 
   return (
     <div>
@@ -20,6 +35,18 @@ export default function IndexGenres() {
       <Link className='btn btn-primary' to='/genres/create'>
         Create a Genre
       </Link>
+
+      <RecordsPerPageSelect
+        onChange={(amountOfRecords) => {
+          setPage(1);
+          setRecordsPerPage(amountOfRecords);
+        }}
+      />
+      <Pagination
+        currentPage={page}
+        totalAmountOfPages={totalAmountOfPages}
+        onChange={(newPage) => setPage(newPage)}
+      />
 
       <GenericList list={genres}>
         <table className='table table-stripe'>
